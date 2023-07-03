bcf84b8546acdca25b337b11b8f44d876774de9a
nadeemk2023 <nadeemekhan@gmail.com>
2023-07-03 11:21:34 -0700

    Added Fetch and Loading Screen while fetching

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import CardList from './CardList.component';
-import { robots } from './robots';
 import SearchBox from './SearchBox.component.js';
 import './App.css';
 
@@ -8,27 +7,43 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchField: '',
     };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ robots: users }));
   }
 
   onSearchChange = e => {
     this.setState({ searchField: e.target.value });
   };
+
   render() {
     const filteredRobots = this.state.robots.filter(robot => {
       return robot.name
         .toLowerCase()
         .includes(this.state.searchField.toLowerCase());
     });
-    return (
-      <div className="tc">
-        <h1 className="f2">Robofriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filteredRobots} />;
-      </div>
-    );
+
+    if (this.state.robots.length === 0) {
+      return (
+        <div className="vh-100 flex justify-center items-center">
+          <h1 className="f2">Loading...</h1>
+        </div>
+      );
+    } else {
+      return (
+        <div className="tc">
+          <h1 className="f2">Robofriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <CardList robots={filteredRobots} />;
+        </div>
+      );
+    }
   }
 }
 
