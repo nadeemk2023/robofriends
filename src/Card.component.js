d801f19b56d4d950825b51757c89e9bcddea7d4b
nadeemk2023 <nadeemekhan@gmail.com>
2023-07-03 01:15:47 -0700

    SearchBox Implemented

---
+++ src/Card.component.js
@@ -0,0 +1,13 @@
+const Card = ({ name, email, id }) => {
+  return (
+    <div className="tc bg-light-green ma2 pa3 dib br3 grow bw2 shadow-5">
+      <img src={`https://robohash.org/${id}?200x200`} alt="" />
+      <div>
+        <h2>{name}</h2>
+        <p>{email}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
---
+++ src/CardList.component.js
@@ -0,0 +1,14 @@
+import Card from './Card.component';
+const CardList = ({ robots }) => {
+  return (
+    <div>
+      {robots.map((user, i) => {
+        return (
+          <Card id={user.id} name={user.name} email={user.email} key={i} />
+        );
+      })}
+    </div>
+  );
+};
+
+export default CardList;
