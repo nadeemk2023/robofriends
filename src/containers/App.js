9fd20a15b7b4eacec946cd381a02a6122b01b5f5
nadeemk2023 <nadeemekhan@gmail.com>
2024-01-29 18:24:45 -0800

    Update title and refactor App component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,51 +1,39 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import CardList from '../components/CardList.component';
 import SearchBox from '../components/SearchBox.component.js';
 import Scroll from '../components/Scroll.component';
 import ErrorBoundary from '../components/ErrorBoundary.component';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      robots: [],
-      searchField: '',
-    };
-  }
+const App = () => {
+  const [robots, setRobots] = useState([]);
+  const [searchField, setSearchField] = useState('');
 
-  componentDidMount() {
+  const filteredRobots = robots.filter(robot => {
+    return robot.name.toLowerCase().includes(searchField.toLowerCase());
+  });
+
+  useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
-      .then(users => this.setState({ robots: users }));
-  }
+      .then(users => setRobots(users));
+  });
 
-  onSearchChange = e => {
-    this.setState({ searchField: e.target.value });
-  };
-
-  render() {
-    const { robots, searchField } = this.state;
-    const filteredRobots = robots.filter(robot => {
-      return robot.name.toLowerCase().includes(searchField.toLowerCase());
-    });
-
-    return !robots.length ? (
-      <div className="vh-100 flex justify-center items-center">
-        <h1 className="f2">Loading...</h1>
-      </div>
-    ) : (
-      <div className="tc">
-        <h1 className="f2">Robofriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <Scroll>
-          <ErrorBoundary>
-            <CardList robots={filteredRobots} />
-          </ErrorBoundary>
-        </Scroll>
-      </div>
-    );
-  }
-}
+  return !robots.length ? (
+    <div className='vh-100 flex justify-center items-center'>
+      <h1 className='f2'>Loading...</h1>
+    </div>
+  ) : (
+    <div className='tc'>
+      <h1 className='f2'>Robofriends</h1>
+      <SearchBox searchChange={e => setSearchField(e.target.value)} />
+      <Scroll>
+        <ErrorBoundary>
+          <CardList robots={filteredRobots} />
+        </ErrorBoundary>
+      </Scroll>
+    </div>
+  );
+};
 
 export default App;
