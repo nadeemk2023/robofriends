5a21ee58457e17d07bcf4457c00d5fe5c13a1741
nadeemk2023 <nadeemekhan@gmail.com>
2024-02-01 23:54:59 -0800

    remove bw2 since there is no border to begin with on the card it seems

--- src/components/Card.component.js
+++ src/components/Card.component.js
@@ -1,7 +1,7 @@
 const Card = ({ name, email, id }) => {
   return (
-    <div className="tc bg-light-green ma2 pa3 dib br3 grow bw2 shadow-5">
-      <img src={`https://robohash.org/${id}?200x200`} alt="" />
+    <div className='tc bg-light-green ma2 pa3 dib br3 grow shadow-5'>
+      <img src={`https://robohash.org/${id}?200x200`} alt='' />
       <div>
         <h2>{name}</h2>
         <p>{email}</p>
